34cb48d04eaf309790cb0daba1393e727ccf0757
Blackie01 <damola24795@gmail.com>
2022-12-28 18:16:17 +0100

    css polish

--- src/components/moreInfo.js
+++ src/components/moreInfo.js
@@ -1,67 +1,65 @@
-import React from 'react'
+import React from "react";
 import "./moreInfo.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { useState } from 'react';
-import Pagination from './pagination';
-import altBook from '../assets/alt-book-image.jpeg';
+import { useState } from "react";
+import Pagination from "./pagination";
+import altBook from "../assets/alt-book-image.jpeg";
 
-const MoreInfo = ({Books}) => {
-    let booksArray = Books.items
+const MoreInfo = ({ Books }) => {
+  let booksArray = Books.items;
   console.log(booksArray);
-    //States for pagination
+  //States for pagination
 
-    const [currentPage, setCurrentPage] = useState(1);
-    const [BooksPerPage] = useState(10);
-    
+  const [currentPage, setCurrentPage] = useState(1);
+  const [BooksPerPage] = useState(10);
 
-    //Pagination Variables
-    const indexOfLastBooks = currentPage * BooksPerPage;
-    const indexOfFirstBooks = indexOfLastBooks - BooksPerPage;
-    const currentBooks = booksArray?.slice(indexOfFirstBooks, indexOfLastBooks);
+  //Pagination Variables
+  const indexOfLastBooks = currentPage * BooksPerPage;
+  const indexOfFirstBooks = indexOfLastBooks - BooksPerPage;
+  const currentBooks = booksArray?.slice(indexOfFirstBooks, indexOfLastBooks);
 
-    const paginate = (pageNumber) => {
-      setCurrentPage(pageNumber)
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
-
-  
 
   return (
     <section>
-        <nav className="navigation-bar">
-        <Link className="logoName" to='/homepage'>
-        <h2 >BookSearch</h2>
+      <nav className="navigation-bar">
+        <Link className="logoName" to="/homepage">
+          <h2>BookSearch</h2>
         </Link>
-            <section>
-                <ul className="nav-right">
-                    <li>About</li>
-                    <li>Contributors</li>
-                    <li>API docs</li>
-                </ul>
-            </section>
-        </nav>
-        <div className="books-container">
+        <section>
+          <ul className="nav-right">
+            <li>About</li>
+            <li>Contributors</li>
+            <li>API docs</li>
+          </ul>
+        </section>
+      </nav>
+      <div className="books-container">
         {currentBooks.map((book, i) => (
-              <div className='books' key={i}>
-                  <img src={book.volumeInfo.imageLinks.thumbnail} alt="book" key={i}/>
-                  <h2>{book.volumeInfo.title}</h2>
-                  <button className="read-more">
-                      <Link to="/moreinfo">
-                        Read More
-                      </Link>
-                  </button>
-              </div>
-            ))}
-        </div>
-        <Pagination
-          Books = {booksArray}
-          totalBooks= {booksArray?.length}
-          currentPage = {currentPage}
-          BooksPerPage = {BooksPerPage}
-          paginate = {paginate}
-        />
+          <div className="books" key={i}>
+            <img
+              src={book.volumeInfo.imageLinks.thumbnail}
+              alt="book"
+              key={i}
+            />
+            <h2>{book.volumeInfo.title}</h2>
+            <button className="read-more">
+              <Link to="/moreinfo">Read More</Link>
+            </button>
+          </div>
+        ))}
+      </div>
+      <Pagination
+        Books={booksArray}
+        totalBooks={booksArray?.length}
+        currentPage={currentPage}
+        BooksPerPage={BooksPerPage}
+        paginate={paginate}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default MoreInfo
-
+export default MoreInfo;
